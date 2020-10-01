ee07057b24a4da6aec35b499833c66fd924ef976
Muhammad Junaid <programming4hacking@gmial.com>
2020-10-01 20:16:01 +0500

    ONE Input Classes

---
+++ PYF/src/core/SettingsError.java
@@ -0,0 +1,21 @@
+package core;
+
+/**
+ * Settings related error
+ *
+ */
+public class SettingsError extends SimError {
+
+	public SettingsError(String cause) {
+		super(cause);
+	}
+
+	public SettingsError(String cause, Exception e) {
+		super(cause,e);
+	}
+
+	public SettingsError(Exception e) {
+		super(e);
+	}
+
+}
